844ceaebd9da540afd8a547bac2ff4a1ca21ac54
Kim Jung <godrm77@gmail.com>
2023-04-20 14:54:46 +0900

    레시피 뷰컨트롤러 추가

--- CornerStore/RecipeViewController.swift
+++ CornerStore/RecipeViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class RecipeViewController: UIViewController {
+    @IBOutlet weak var selectedTitle: UILabel!
+    @IBOutlet weak var selectedImage: UIImageView!
+    @IBOutlet weak var recipeText: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
