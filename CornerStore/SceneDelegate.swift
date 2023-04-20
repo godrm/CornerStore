846d348a4c444fe3ae97014ae1a5299c964ca505
Kim Jung <godrm77@gmail.com>
2023-04-20 11:15:51 +0900

    프로젝트 초기 설정
    프로젝트 설정 완료

---
+++ CornerStore/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  CornerStore
+//
+//  Created by JK on 2023/04/20.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
---
+++ CornerStore/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CornerStore
+//
+//  Created by JK on 2023/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
