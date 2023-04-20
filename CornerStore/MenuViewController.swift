8024603e37ec40387fd2a15b1c793f6f051c374a
Kim Jung <godrm77@gmail.com>
2023-04-20 12:07:34 +0900

    탭바 화면 구성
    화면별 뷰 컨트롤러 추가하기

---
+++ CornerStore/MenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CornerStore
+//
+//  Created by JK on 2023/04/20.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
