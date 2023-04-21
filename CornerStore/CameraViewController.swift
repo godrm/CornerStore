48f07dae8f69d33f0700209780074f7103fdd319
Kim Jung <godrm77@gmail.com>
2023-04-21 09:05:38 +0900

    카메라 처리 코드 추가

--- CornerStore/CameraViewController.swift
+++ CornerStore/CameraViewController.swift
@@ -6,17 +6,117 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController,      AVCapturePhotoCaptureDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
+
     @IBOutlet weak var preview: UIView!
     @IBOutlet weak var itemResult: UILabel!
     @IBOutlet weak var recommendButton: UIButton!
+    @IBOutlet weak var guideLabel: UILabel!
+    
+    private let captureSession = AVCaptureSession()
+    private let photoOutput = AVCapturePhotoOutput()
+
+    private var bufferSize: CGSize = .zero
+    private let videoDataOutput = AVCaptureVideoDataOutput()
+    private let videoDataOutputQueue = DispatchQueue(label: "VideoDataOutput", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+#if targetEnvironment(simulator)
+        guideLabel.isHidden = false
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(tapPreview))
+        preview.addGestureRecognizer(recognizer)
+#else
+        guideLabel.isHidden = true
+        initSession()
+        addPreview()
+        captureSession.startRunning()
+#endif
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    }
+        
+    func initSession() {
+        captureSession.beginConfiguration()
+
+        let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                                  for: .video, position: .back)
+        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!),
+                captureSession.canAddInput(videoDeviceInput)
+            else { return }
+        captureSession.addInput(videoDeviceInput)
+
+        let settings = AVCapturePhotoSettings()
+        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+                                  kCVPixelBufferWidthKey as String: 1024,
+                                  kCVPixelBufferHeightKey as String: 1024]
+        settings.previewPhotoFormat = previewFormat
+        photoOutput.capturePhoto(with:  settings, delegate: self)
+        
+        guard captureSession.canAddOutput(photoOutput) else { return }
+        captureSession.sessionPreset = .high
+        captureSession.addOutput(photoOutput)
+
+        guard captureSession.canAddOutput(videoDataOutput) else { return }
+        videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+        videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+        
+        let captureConnection = videoDataOutput.connection(with: .video)
+        // Always process the frames
+        captureConnection?.isEnabled = true
+        do {
+            try  videoDevice!.lockForConfiguration()
+            let dimensions = CMVideoFormatDescriptionGetDimensions((videoDevice?.activeFormat.formatDescription)!)
+            bufferSize.width = CGFloat(dimensions.width)
+            bufferSize.height = CGFloat(dimensions.height)
+            videoDevice!.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+
+        captureSession.commitConfiguration()
     }
 
-    @IBAction func touchedRecommend(_ sender: Any) {
+    func addPreview() {
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        let rootLayer = preview.layer
+        previewLayer.frame = rootLayer.bounds
+        rootLayer.addSublayer(previewLayer)
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto
+                        photo: AVCapturePhoto, error: Error?) {
+        if let dataImage = photo.fileDataRepresentation() {
+            let dataProvider = CGDataProvider(data: dataImage as CFData)
+            let cgImageRef: CGImage = CGImage(jpegDataProviderSource: dataProvider!,
+                            decode: nil, shouldInterpolate: true, intent: .defaultIntent)!
+            let image = UIImage.init(cgImage: cgImageRef)
+
+        }
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "recommend" else { return }
+        
+    }
+
+    @objc private func tapPreview(rec: UITapGestureRecognizer) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true)
     }
 }
 
+extension CameraViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+    }
+}
